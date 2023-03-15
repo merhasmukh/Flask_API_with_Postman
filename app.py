e409aea679f07f0c7670148a4545ece50220d602
mhgn <mhgn0001@gmail.com>
2023-03-15 17:12:32 +0530

    simple flask api

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-print("hi")
-print("hi")
+from flask import Flask
+
+
+app=Flask(__name__)
+
+
+@app.route("/",methods=["POST","GET"])
+def main():
+    return "hi"
+
+if __name__=="__main__":
+    main()
+    app.run('0.0.0.0')
